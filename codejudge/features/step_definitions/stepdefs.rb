efbcf9137fba618f3d3625e0fa5b74da50c4d9cb
theraj C9 <theraj@tamu.edu>
2022-03-09 23:59:42 +0000

    added pending step definitions

---
+++ codejudge/features/step_definitions/stepdefs.rb
@@ -0,0 +1,21 @@
+# https://ucumber.io/docs/guides/10-minute-tutorial/
+
+Given('the admin provides correct credentials') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('they click on the login button') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('they should get a positive feedback') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('the admin provides incorrect credentials') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('they should get a negative feedback') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
