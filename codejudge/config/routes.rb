4853b35265908dfd6e4907efd303725d0a1542f7
noceo <paulschade20@gmail.com>
2022-03-27 16:25:14 -0500

    added problems page

--- codejudge/config/routes.rb
+++ codejudge/config/routes.rb
@@ -1,5 +1,6 @@
 
 Rails.application.routes.draw do
+  resources :problems
   get 'errors/not_found'
 
   get 'errors/internal_server_error'
---
+++ codejudge/test/system/problems_test.rb
@@ -0,0 +1,39 @@
+require "application_system_test_case"
+
+class ProblemsTest < ApplicationSystemTestCase
+  setup do
+    @problem = problems(:one)
+  end
+
+  test "visiting the index" do
+    visit problems_url
+    assert_selector "h1", text: "Problems"
+  end
+
+  test "should create problem" do
+    visit problems_url
+    click_on "New problem"
+
+    click_on "Create Problem"
+
+    assert_text "Problem was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Problem" do
+    visit problem_url(@problem)
+    click_on "Edit this problem", match: :first
+
+    click_on "Update Problem"
+
+    assert_text "Problem was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Problem" do
+    visit problem_url(@problem)
+    click_on "Destroy this problem", match: :first
+
+    assert_text "Problem was successfully destroyed"
+  end
+end
