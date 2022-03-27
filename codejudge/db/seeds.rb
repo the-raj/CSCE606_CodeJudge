f146735ce77f8f10da055bbb3738785944c1d586
noceo <paulschade20@gmail.com>
2022-03-27 17:54:55 -0500

    updated seed file for db

--- codejudge/db/seeds.rb
+++ codejudge/db/seeds.rb
@@ -6,4 +6,17 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(username: "admin", password: "admin", password_confirmation: "admin", firstname: "admin", lastname: "admin", email: "admin@email.com", registration_time: DateTime.now, user_role: 0)
+#User.create(username: "admin", password: "admin", password_confirmation: "admin", firstname: "admin", lastname: "admin", email: "admin@email.com", registration_time: DateTime.now, user_role: 0)
+
+#TEST USERS
+
+User.create(username: "admin", password: "admin", firstname: "admin", lastname: "admin", email: "admin@admin.com", registration_time: DateTime.now, user_role: 0)
+User.create(username: "testuser", password: "password", firstname: "Test", lastname: "User", email: "testuser@example.com", registration_time: DateTime.now, user_role: 2)
+User.create(username: "testinstructor", password: "password", firstname: "Test", lastname: "Instructor", email: "testinstructor@example.com", registration_time: DateTime.now, user_role: 1)
+User.create(username: "testTA", password: "password", firstname: "Test", lastname: "TA", email: "testTA@example.com", registration_time: DateTime.now, user_role: 1) 
+
+
+
+#TEST PROBLEMS
+
+Problem.create(problem_text: "Output should contain one line, containing the string 'Hello World!'.", number_test_cases: 1)
