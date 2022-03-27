deffb98efbe35c15b8424356168863be1091aabf
noceo <paulschade20@gmail.com>
2022-03-27 17:41:59 -0500

    finished test cases for user page

--- codejudge/features/step_definitions/stepdefs.rb
+++ codejudge/features/step_definitions/stepdefs.rb
@@ -5,23 +5,24 @@ Given('the admin is on the login page') do
 end
 
 Given('the admin provides correct credentials') do
-  @browser.find_element_by_id("username").send_keys("admin")
-  @browser.find_element_by_id("password").send_keys("admin")
+  @browser.find_element(:id, "username").send_keys("admin")
+  @browser.find_element(:id, "password").send_keys("admin")
 end
 
 When('they click on the login button') do
-  @browser.find_element_by_id("login_btn").click()
+  @browser.find_element(:id, "login_btn").click()
+  sleep(1)
 end
 
 Then('they should get a positive feedback') do
-  raise "Fail" if @browser.find_element_by_id("notice").innerHTML != "Logged in successfully"
+  raise "Fail" if @browser.find_element(:id, "notice").text != "Logged in successfully"
 end
 
 Given('the admin provides incorrect credentials') do
-  @browser.find_element_by_id("username").send_keys("admin")
-  @browser.find_element_by_id("password").send_keys("incorrect")
+  @browser.find_element(:id, "username").send_keys("admin")
+  @browser.find_element(:id, "password").send_keys("incorrect")
 end
 
 Then('they should get a negative feedback') do
-  raise "Fail" if @browser.find_element_by_id("notice").innerHTML != "Incorrect username or password"
+  raise "Fail" if @browser.find_element(:id, "notice").text != "Incorrect username or password"
 end 
