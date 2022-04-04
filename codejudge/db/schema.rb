d54742315113553e9183741ae570e997f009ddfa
Laren Spear <53915404+larenspear@users.noreply.github.com>
2022-04-04 16:41:46 -0500

    Added more columns to users

--- codejudge/db/schema.rb
+++ codejudge/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_04_210600) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_04_213724) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_04_210600) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.string "username"
+    t.string "password"
+    t.string "firstname"
+    t.string "lastname"
   end
 
   add_foreign_key "test_cases", "problems"
