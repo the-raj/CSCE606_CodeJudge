246cac46c95550054cef2faf690c469bf63615bd
Laren Spear <53915404+larenspear@users.noreply.github.com>
2022-04-04 16:52:39 -0500

    Seeds work, for now

--- codejudge/db/schema.rb
+++ codejudge/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_04_213724) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_04_214523) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_04_213724) do
     t.string "password"
     t.string "firstname"
     t.string "lastname"
+    t.string "password_digest"
   end
 
   add_foreign_key "test_cases", "problems"
--- codejudge/db/seeds.rb
+++ codejudge/db/seeds.rb
@@ -12,10 +12,10 @@ require 'faker'
 
 #TEST USERS
 
-User.create(username: "admin", password: "admin", firstname: "admin", lastname: "admin", email: "admin@admin.com", registration_time: DateTime.now, user_role: 0)
-User.create(username: "testuser", password: "password", firstname: "Test", lastname: "User", email: "testuser@example.com", registration_time: DateTime.now, user_role: 2)
-User.create(username: "testinstructor", password: "password", firstname: "Test", lastname: "Instructor", email: "testinstructor@example.com", registration_time: DateTime.now, user_role: 1)
-User.create(username: "testTA", password: "password", firstname: "Test", lastname: "TA", email: "testTA@example.com", registration_time: DateTime.now, user_role: 1) 
+User.create(username: "admin", password: "admin", firstname: "admin", lastname: "admin", email: "admin@admin.com")
+User.create(username: "testuser", password: "password", firstname: "Test", lastname: "User", email: "testuser@example.com")
+User.create(username: "testinstructor", password: "password", firstname: "Test", lastname: "Instructor", email: "testinstructor@example.com")
+User.create(username: "testTA", password: "password", firstname: "Test", lastname: "TA", email: "testTA@example.com") 
 
 (1..50).each do |id|
   User.create(
@@ -23,18 +23,16 @@ User.create(username: "testTA", password: "password", firstname: "Test", lastnam
     password: Faker::Alphanumeric.alphanumeric(number: 10),
     firstname: Faker::Name.first_name,
     lastname: Faker::Name.last_name,
-    email: Faker::Internet.email,
-    registration_time: DateTime.now,
-    user_role: 2
+    email: Faker::Internet.email
   )
 end
 
 #TEST PROBLEMS
 
-Problem.create(problem_text: "Output should contain one line, containing the string 'Hello World!'.")
+Problem.create(body: "Output should contain one line, containing the string 'Hello World!'.")
 
 (1..50).each do |id|
   Problem.create(
-    problem_text: Faker::Lorem.paragraph(sentence_count: 5)
+    body: Faker::Lorem.paragraph(sentence_count: 5)
   )
 end
