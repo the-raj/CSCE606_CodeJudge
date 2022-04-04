e8a23e0d9f2d5583cae04471995b13d8bee5613c
Laren Spear <53915404+larenspear@users.noreply.github.com>
2022-04-04 09:26:40 -0500

    No longer using structure.sql

--- codejudge/config/application.rb
+++ codejudge/config/application.rb
@@ -10,7 +10,6 @@ module Codejudge
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.active_record.schema_format = :sql
     config.exceptions_app = self.routes
 
     # Configuration for the application, engines, and railties goes here.
