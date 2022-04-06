302712884eef5937f51becf66c3833eca133f2ef
Laren Spear <53915404+larenspear@users.noreply.github.com>
2022-04-06 17:46:05 -0500

    Add role model

--- codejudge/db/schema.rb
+++ codejudge/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_06_222956) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_06_223427) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_06_222956) do
   create_table "problems", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
