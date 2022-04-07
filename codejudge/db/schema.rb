626791790c297213f1e5d35168c0bcfefe0fbfe7
Laren Spear <53915404+larenspear@users.noreply.github.com>
2022-04-07 17:53:22 -0500

    Clean up attempts table

--- codejudge/db/schema.rb
+++ codejudge/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_07_223422) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_07_223812) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,8 +24,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_07_223422) do
   end
 
   create_table "attempts", force: :cascade do |t|
-    t.bigint "user"
-    t.bigint "problem"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
