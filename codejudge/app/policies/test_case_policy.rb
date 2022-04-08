5e601175dd4007842bacf669021cad36f0a458d8
Laren Spear <53915404+larenspear@users.noreply.github.com>
2022-04-07 21:19:12 -0500

    Add test case policy

--- codejudge/app/policies/test_case_policy.rb
+++ codejudge/app/policies/test_case_policy.rb
@@ -5,15 +5,23 @@ class TestCasePolicy < ApplicationPolicy
   end
 
   def delete?
-    ##
+    user.role? :admin or user.role? :instructor or user.role? :ta 
   end
 
   def update?
-    ##
+    user.role? :admin or user.role? :instructor or user.role? :ta 
   end
 
   def destroy?
-    ##
+    user.role? :admin or user.role? :instructor or user.role? :ta 
+  end
+
+  def index?
+    user.role? :admin or user.role? :instructor or user.role? :ta 
+  end
+
+  def show?
+    user.role? :admin or user.role? :instructor or user.role? :ta or user.role? :student
   end
   
 end
