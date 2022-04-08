c32443f25b18b87b4df597deabda4d052ec5c83a
Laren Spear <53915404+larenspear@users.noreply.github.com>
2022-04-07 21:06:33 -0500

    Wrote problem policy

--- codejudge/app/policies/problem_policy.rb
+++ codejudge/app/policies/problem_policy.rb
@@ -5,11 +5,19 @@ class ProblemPolicy < ApplicationPolicy
   end
 
   def destroy?
-    ##
+    user.role? :admin or user.role? :instructor or user.role? :ta
   end
 
   def update?
-    ##
+    user.role? :admin or user.role? :instructor or user.role? :ta
+  end
+
+  def index?
+    user.role? :admin or user.role? :instructor or user.role? :ta or user.role? :student
+  end
+
+  def show?
+    user.role? :admin or user.role? :instructor or user.role? :ta or user.role? :student
   end
   
 end
