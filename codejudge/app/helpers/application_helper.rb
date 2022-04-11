83480bee16efba277fe3343fe18e9cbda235b474
noceo <paulschade20@gmail.com>
2022-04-10 19:48:45 -0500

    fixed production asset issue

--- codejudge/app/helpers/application_helper.rb
+++ codejudge/app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def embedded_svg(filename, options = {})
-    assets = Rails.application.assets
+    assets = Rails.application.assets || ::Sprockets::Railtie.build_environment(Rails.application)
     file = assets.find_asset(filename).source.force_encoding("UTF-8")
     doc = Nokogiri::HTML::DocumentFragment.parse file
     svg = doc.at_css "svg"
--- codejudge/db/migrate/20220404213231_fix_users.rb
+++
@@ -1,8 +0,0 @@
-class FixUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :user, :username, :string
-    add_column :user, :password, :string
-    add_column :user, :firstname, :string
-    add_column :user, :lastname, :string
-  end
-end
