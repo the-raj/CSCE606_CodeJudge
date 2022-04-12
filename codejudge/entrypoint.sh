c6168a990ecab38821ad51d640fa4ac66d852d6e
noceo <paulschade20@gmail.com>
2022-04-12 16:57:10 -0500

    updated Dockerfile to remove server.pid before every container start

--- codejudge/entrypoint.sh
+++ codejudge/entrypoint.sh
@@ -2,7 +2,9 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /code/tmp/pids/server.pid
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
-exec "$@"
+exec bundle exec "$@"
