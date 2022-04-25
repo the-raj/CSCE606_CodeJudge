b7ada9f16987a04e52cb3dcb47f0659536b1bd58
noceo <paulschade20@gmail.com>
2022-04-25 11:40:58 -0500

    refactored embedded svg helper

--- codejudge/app/helpers/application_helper.rb
+++ codejudge/app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def embedded_svg(filename, options = {})
     assets = Rails.application.assets || ::Sprockets::Railtie.build_environment(Rails.application)
-    file = assets.find_asset(filename).source.force_encoding("UTF-8")
+    file = assets.find_asset("svg/" + filename + ".svg").source.force_encoding("UTF-8")
     doc = Nokogiri::HTML::DocumentFragment.parse file
     svg = doc.at_css "svg"
     if options[:class].present?
