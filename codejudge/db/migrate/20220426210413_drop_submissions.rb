8d5f63e898407422df244b59c616b1828fe0785c
noceo <paulschade20@gmail.com>
2022-05-04 13:22:15 -0500

    removed unnecessary migration

--- codejudge/db/migrate/20220426210413_drop_submissions.rb
+++
@@ -1,4 +0,0 @@
-class DropSubmissions < ActiveRecord::Migration[7.0]
-  def change
-    drop table :submissions
-  end
--- codejudge/db/migrate/20220426210849_drop_table_submissions.rb
+++
@@ -1,5 +0,0 @@
-class DropTableSubmissions < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :submissions
-  end
-end
