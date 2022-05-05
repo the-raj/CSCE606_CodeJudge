12dac82491d89e8c99c5af0408bd451a7b1f4adb
noceo <paulschade20@gmail.com>
2022-05-04 22:44:52 -0500

    students can only view example test cases

--- codejudge/app/models/problem.rb
+++ codejudge/app/models/problem.rb
@@ -1,4 +1,8 @@
 class Problem < ApplicationRecord
   has_many :test_cases, dependent: :destroy
   accepts_nested_attributes_for :test_cases, reject_if: :all_blank, allow_destroy: true
+
+  def visible_test_cases(problem, role)
+    role == :student.to_s ? problem.test_cases.where(example: true) : problem.test_cases
+  end
 end
