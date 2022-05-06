9150a483b66f7a555f504e796cdae6d7829c4e93
Laren Spear <53915404+larenspear@users.noreply.github.com>
2022-05-05 22:26:03 -0500

    Add attempts_test_cases join table

--- codejudge/app/models/attempt.rb
+++ codejudge/app/models/attempt.rb
@@ -2,5 +2,5 @@ class Attempt < ApplicationRecord
   belongs_to :language
   belongs_to :user
   belongs_to :problem
-
+  has_and_belongs_to_many :test_cases
 end
--- codejudge/db/schema.rb
+++ codejudge/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_05_214305) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_06_032050) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_05_214305) do
     t.index ["language_id"], name: "index_attempts_on_language_id"
     t.index ["problem_id"], name: "index_attempts_on_problem_id"
     t.index ["user_id"], name: "index_attempts_on_user_id"
+  end
+
+  create_table "attempts_test_cases", id: false, force: :cascade do |t|
+    t.bigint "attempt_id"
+    t.bigint "test_case_id"
+    t.index ["attempt_id"], name: "index_attempts_test_cases_on_attempt_id"
+    t.index ["test_case_id"], name: "index_attempts_test_cases_on_test_case_id"
   end
 
   create_table "groups", force: :cascade do |t|
