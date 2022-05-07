30655c28acc4602496d8e5123ef7889b95b56f16
Laren Spear <53915404+larenspear@users.noreply.github.com>
2022-05-07 17:06:26 -0500

    Removed attempts testcases join table

--- codejudge/db/schema.rb
+++ codejudge/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_07_215853) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_07_220429) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,16 +33,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_07_215853) do
     t.index ["language_id"], name: "index_attempts_on_language_id"
     t.index ["problem_id"], name: "index_attempts_on_problem_id"
     t.index ["user_id"], name: "index_attempts_on_user_id"
-  end
-
-  create_table "attempts_test_cases", id: false, force: :cascade do |t|
-    t.bigint "attempt_id"
-    t.bigint "test_case_id"
-    t.boolean "passed"
-    t.text "stdout"
-    t.text "stderr"
-    t.index ["attempt_id"], name: "index_attempts_test_cases_on_attempt_id"
-    t.index ["test_case_id"], name: "index_attempts_test_cases_on_test_case_id"
   end
 
   create_table "groups", force: :cascade do |t|
