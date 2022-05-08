cfd6b679a4cdd493c4ee2584c44a84ceee04a508
noceo <paulschade20@gmail.com>
2022-05-08 13:52:12 -0500

    added dark mode and color switch

--- codejudge/app/javascript/controllers/application.js
+++ codejudge/app/javascript/controllers/application.js
@@ -4,7 +4,18 @@ const application = Application.start();
 
 // TODO: Move the attribute change over to the template as soon as the preferred theme gets saved for a user in the database
 document.addEventListener("turbo:load", () => {
-  document.body.classList.add("light");
+  const body = document.body
+  body.classList.add("light")
+  const layoutButton = document.getElementById("color-mode-button")
+  layoutButton.addEventListener("click", () => {
+    if (body.classList.contains("light")) {
+      body.classList.remove("light")
+      body.classList.add("dark")
+    } else {
+      body.classList.remove("dark")
+      body.classList.add("light")
+    }
+  })
 });
 
 // Configure Stimulus development experience
